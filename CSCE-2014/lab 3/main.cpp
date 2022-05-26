6fa7742998c572953ce4e35276e4abb72762f86c
ethan-may <106100518+ethan-may@users.noreply.github.com>
2022-05-26 12:37:14 -0500

    Create main.cpp

---
+++ CSCE-2014/lab 3/main.cpp
@@ -0,0 +1,41 @@
+//----------------------------------------------
+// Purpose: Numbers class with basic operations
+// Author:  Ethan May
+//----------------------------------------------
+
+#include "numbers.h"
+
+//----------------------------------------------
+int main()
+{
+   Numbers num;
+   string filename;
+
+   cout << "Enter filename:\n";
+   filename = "numbers.txt";
+   // cin >> filename;
+   num.readFile(filename);
+
+   cout << "data = ";
+   for (int index = 0; index < num.getCount(); index++)
+      cout << num.getValue(index) << " ";
+   cout << endl;
+
+   DataType count = num.getCount();
+   cout << "count = " << count << endl;
+
+   DataType min = num.findMin();
+   cout << "min = " << min << endl;
+
+   DataType max = num.findMax();
+   cout << "max = " << max << endl;
+
+   DataType mean = num.findMean();
+   cout << "mean = " << mean << endl;
+   
+   DataType variance = num.findVariance();
+   cout << "variance = " << variance << endl;
+   
+   DataType sd = num.findStandardDeviation();
+   cout << "standard deviation = " << sd << endl;
+}
